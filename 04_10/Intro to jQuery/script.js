d398f5b55f319d12716d574c8950d85cfc9898bf
andrasfeketeprivat <schwarz.bence@gmail.com>
2020-04-10 17:17:00 +0200

    Intro to jQuery

---
+++ 04_10/Intro to jQuery/script.js
@@ -0,0 +1,6 @@
+// Change the text of the first blue box from Velox to Macrotis
+$('#fox1').text("Macaronis")
+// Change the border color of the second from blue to purple
+$('#fox2').css("border-color","purple")
+// Change the background color of all 3 blue boxes to yellow
+$("#fox3").css("background-color","yellow")
